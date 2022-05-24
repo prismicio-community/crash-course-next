d4c27a564a08b2018a72c35022889e976a6efc6e
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-05-24 12:48:20 -1000

    feat: add Slice models

---
+++ pages/slice-simulator.js
@@ -0,0 +1,22 @@
+import { SliceSimulator } from "@prismicio/slice-simulator-react";
+import { SliceZone } from "@prismicio/react";
+
+import state from "../.slicemachine/libraries-state.json";
+import { components } from "../slices";
+
+const SliceSimulatorPage = () => (
+  <SliceSimulator
+    sliceZone={(props) => <SliceZone {...props} components={components} />}
+    state={state}
+  />
+);
+
+export default SliceSimulatorPage;
+
+export const getStaticProps = async () => {
+  if (process.env.NODE_ENV === "production") {
+    return { notFound: true };
+  } else {
+    return { props: {} };
+  }
+};
